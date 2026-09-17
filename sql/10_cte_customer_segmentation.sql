b47a3d5f77ac04f7f2f755e16369242c78b081dc
Mario Antuña <mantuna@cmi.mx>
2026-09-17 15:30:12 -0600

    Scripts_CTE

---
+++ sql/10_cte_customer_segmentation.sql
@@ -0,0 +1,29 @@
+USE DataEngineeringPractice;
+GO
+
+WITH total_por_cliente AS (
+    SELECT
+        c.id_cliente,
+        c.nombre,
+        SUM(v.cantidad * p.precio) AS total_comprado
+    FROM Ventas v
+    INNER JOIN Clientes c
+        ON v.id_cliente = c.id_cliente
+    INNER JOIN Productos p
+        ON v.id_producto = p.id_producto
+    GROUP BY
+        c.id_cliente,
+        c.nombre
+)
+
+SELECT
+    id_cliente,
+    nombre,
+    total_comprado,
+    CASE
+        WHEN total_comprado >= 20000 THEN 'Alto valor'
+        WHEN total_comprado >= 15000 THEN 'Valor medio'
+        ELSE 'Valor bajo'
+    END AS segmento
+FROM total_por_cliente
+ORDER BY total_comprado DESC;
